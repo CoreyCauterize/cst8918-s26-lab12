f9a11cdef6c1b262440a9f4879a88cfbf2a5038d
JChen-AC <chen1101@algonquinlive.com>
2026-07-30 01:38:11 -0400

    Adding descriptions to outputs

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -27,4 +27,5 @@ resource "azurerm_resource_group" "rg" {
 
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
+  description = "tf-app : Resource Group Name"
 }
--- infra/tf-backend/main.tf
+++ infra/tf-backend/main.tf
@@ -72,14 +72,19 @@ resource "azurerm_storage_container" "container" {
 
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
+  description = "tf-backend : Resource Group Name"
+
 }
 output "storage_account_name" {
   value = azurerm_storage_account.storage.name
+  description = "tf-backend : Storage Account Name"
 }
 output "container_name" {
   value = azurerm_storage_container.container.name
+  description = "tf-backend : Container Name"
 }
 output "arm_access_key" {
   value = azurerm_storage_account.storage.primary_access_key
+  description = "tf-backend : ARM Access Key"
   sensitive=true
 }
